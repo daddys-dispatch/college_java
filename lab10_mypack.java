1d788e6bc6f741a52c26eaefa9cc70c383bc7228
Daddy <daddys.dispatch@gmail.com>
2024-12-12 02:41:09 +0530

    Create lab10_mypack.java

---
+++ lab10_mypack.java
@@ -0,0 +1,10 @@
+package daddy;
+
+public class MyPackageClass {
+    public void displayMessage() {
+        System.out.println("Hello from MyPackageClass in daddy package!");
+    }
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
