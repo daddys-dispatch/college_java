db9757b9a1e949668f0dc9d0a3904b5164b152a0
Daddy <daddys.dispatch@gmail.com>
2024-12-12 02:09:36 +0530

    Update lab7.java

--- lab7.java
+++ lab7.java
@@ -6,43 +6,23 @@ interface Resizable {
 }
 
 class Rectangle implements Resizable {
-    private int width;
-    private int height;
-    public Rectangle(int width, int height) { 
-        this.width = width; 
-        this.height = height; 
-    }
+    private int width, height;
+    public Rectangle(int width, int height) { this.width = width; this.height = height; }
 
     @Override
-    public void resizeWidth(int width) {
-        this.width = width;
-        System.out.println("Resized width to: " + width);
-    }
-
+    public void resizeWidth(int width) { this.width = width; }
     @Override
-    public void resizeHeight(int height) {
-        this.height = height;
-        System.out.println("Resized height to: " + height);
-    }
+    public void resizeHeight(int height) { this.height = height; }
 
-    public int getWidth() { return width; }
-    public int getHeight() { return height; }
-
-    public void displayInfo() {
-        System.out.println("Rectangle: Width = " + width + ", Height = " + height);
-    }
+    void display() { System.out.println("Rectangle: Width = " + width + ", Height = " + height); }
 }
 
 public class ResizeDemo {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(10, 5);
-        System.out.println("Original Rectangle Info:");
-        rectangle.displayInfo();
-        
-        rectangle.resizeWidth(15);
-        rectangle.resizeHeight(8);
-        
-        System.out.println("\nUpdated Rectangle Info:");
-        rectangle.displayInfo();
+        Rectangle rect = new Rectangle(10, 5);
+        rect.display();
+        rect.resizeWidth(15);
+        rect.resizeHeight(8);
+        rect.display();
     }
 }
